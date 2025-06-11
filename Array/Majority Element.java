b99a5958be8b1b68939c2a9332cc47915dd8eae9
SarthakRathi <sarthakrathi89@gmail.com>
2025-06-11 22:49:24 +0530

    better majority element

---
+++ Array/Majority Element.java
@@ -0,0 +1,29 @@
+// Better
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Majority {
+    public static void main(String[] args) {
+        int[] nums = { 3, 2, 3 };
+
+        Map<Integer, Integer> hash = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (hash.containsKey(nums[i])) {
+                hash.put(nums[i], hash.get(nums[i]) + 1);
+            } else {
+                hash.put(nums[i], 1);
+            }
+        }
+
+        for (Map.Entry<Integer, Integer> entry : hash.entrySet()) {
+            if (entry.getValue() > nums.length / 2) {
+                System.out.println(entry.getKey());
+            }
+        }
+    }
+}
+
+// Time Complexity = O(n)
+// Space Complexity = O(n)
