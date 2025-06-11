0ba2cf4184b7af69082c3f9f6964bd6a08d9b68f
SarthakRathi <sarthakrathi89@gmail.com>
2025-06-11 13:10:24 +0530

    better brute logest subarray

--- Array/Longest Subarray with sum K.java
+++ Array/Longest Subarray with sum K.java
@@ -3,20 +3,18 @@
 class LongestSubarray {
     public static void main(String[] args) {
         int[] nums = { 1, 2, 3, 1, 1, 1, 1, 4, 2, 3 };
-        int s = 5;
+        int s = 4;
 
         int max = 0;
         int sum = 0;
         int count = 0;
 
         for (int i = 0; i < nums.length; i++) {
+            count = 0;
+            sum = 0;
             for (int j = i; j < nums.length; j++) {
-                count = 0;
-                sum = 0;
-                for (int k = i; k <= j; k++) {
-                    sum += nums[k];
-                    count++;
-                }
+                sum += nums[j];
+                count++;
                 if (sum == s) {
                     if (max < count) {
                         max = count;
