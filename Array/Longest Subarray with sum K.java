741bddc78d0f2a305ff49123c96c7658cbd1087c
SarthakRathi <sarthakrathi89@gmail.com>
2025-06-11 14:08:52 +0530

    better longest subarray

--- Array/Longest Subarray with sum K.java
+++ Array/Longest Subarray with sum K.java
@@ -1,30 +1,68 @@
-//Bruteforce
+//Better
+
+import java.util.HashMap;
+import java.util.Map;
 
 class LongestSubarray {
     public static void main(String[] args) {
-        int[] nums = { 1, 2, 3, 1, 1, 1, 1, 4, 2, 3 };
-        int s = 4;
+        int[] nums = { 2, 0, 0, 3 };
+        int s = 3;
 
-        int max = 0;
         int sum = 0;
-        int count = 0;
+        int length = 0;
+        int temp = 0;
 
+        Map<Integer, Integer> hash = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            count = 0;
-            sum = 0;
-            for (int j = i; j < nums.length; j++) {
-                sum += nums[j];
-                count++;
-                if (sum == s) {
-                    if (max < count) {
-                        max = count;
-                    }
-                }
+
+            sum += nums[i];
+
+            if (hash.containsKey(sum - s)) {
+                temp = i - hash.get(sum - s);
+            }
+
+            if (temp > length) {
+                length = temp;
+            }
+
+            if (!hash.containsKey(sum)) {
+                hash.put(sum, i); // For zeroes
             }
         }
-        System.out.print(max);
+        System.out.println(length);
     }
 }
+
+// Time Complexity = O(n2)
+// Space Complexity = O(n)
+
+// Bruteforce
+
+// class LongestSubarray {
+// public static void main(String[] args) {
+// int[] nums = { 1, 2, 3, 1, 1, 1, 1, 4, 2, 3 };
+// int s = 4;
+
+// int max = 0;
+// int sum = 0;
+// int count = 0;
+
+// for (int i = 0; i < nums.length; i++) {
+// count = 0;
+// sum = 0;
+// for (int j = i; j < nums.length; j++) {
+// sum += nums[j];
+// count++;
+// if (sum == s) {
+// if (max < count) {
+// max = count;
+// }
+// }
+// }
+// }
+// System.out.print(max);
+// }
+// }
 
 // Time Complexity = O(n**2)
 // Space Complexity = O(1)
