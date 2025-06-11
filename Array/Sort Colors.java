eb43dfbec913ada91c9cdc1e07885f4b526bc3eb
SarthakRathi <sarthakrathi89@gmail.com>
2025-06-11 22:13:19 +0530

    optimal sort colors

--- Array/Sort Colors.java
+++ Array/Sort Colors.java
@@ -4,13 +4,35 @@ class Colors {
     public static void main(String[] args) {
         int[] nums = { 0, 1, 2, 0, 1, 2, 1, 2, 0, 0, 0, 1 };
 
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+        int temp;
+
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                temp = nums[high];
+                nums[high] = nums[mid];
+                nums[mid] = temp;
+                high--;
+            }
+        }
+
         for (int i : nums) {
             System.out.println(i);
         }
     }
 }
 
-// Time Complexity = O(2n)
+// Time Complexity = O(n)
 // Space Complexity = O(1)
 
 // Better
