2650483eb15a19d8d6b7900b5dc89aca74801f06
SarthakRathi <sarthakrathi89@gmail.com>
2025-06-12 21:53:46 +0530

    rearrange array elements

---
+++ Array/Rearrange Array Elements by Sign.java
@@ -0,0 +1,28 @@
+// Optimal
+
+class Rearrange {
+    public static void main(String[] args) {
+        int[] nums = { 3, 1, -2, -5, 2, -4 };
+
+        int[] ans = new int[nums.length];
+        int pos = 0;
+        int neg = 1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                ans[pos] = nums[i];
+                pos = pos + 2;
+            } else {
+                ans[neg] = nums[i];
+                neg = neg + 2;
+            }
+        }
+
+        for (int i : ans) {
+            System.out.println(i);
+        }
+    }
+}
+
+// Time Complexity = O(n)
+// Space Complexity = O(n)
