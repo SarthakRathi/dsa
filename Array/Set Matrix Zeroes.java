6dc9aea7e5e9169d87464d266f9f26d518074984
SarthakRathi <sarthakrathi89@gmail.com>
2025-06-20 19:50:53 +0530

    optimal matrix zeros

--- Array/Set Matrix Zeroes.java
+++ Array/Set Matrix Zeroes.java
@@ -1,4 +1,4 @@
-// Better
+// Optimal
 
 class Matrix {
     public static void main(String[] args) {
@@ -7,24 +7,40 @@ class Matrix {
 
         int rows = matrix.length;
         int cols = matrix[0].length;
-
-        boolean[] zeroRows = new boolean[rows];
-        boolean[] zeroCols = new boolean[cols];
+        int col0 = 1;
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 if (matrix[i][j] == 0) {
-                    zeroRows[i] = true;
-                    zeroCols[j] = true;
+                    matrix[i][0] = 0;
+                    if (j != 0) {
+                        matrix[0][j] = 0;
+                    } else {
+                        col0 = 0;
+                    }
                 }
             }
         }
 
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (zeroRows[i] || zeroCols[j]) {
-                    matrix[i][j] = 0;
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][j] != 0) {
+                    if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                        matrix[i][j] = 0;
+                    }
                 }
+            }
+        }
+
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < cols; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+
+        if (col0 == 0) {
+            for (int i = 0; i < rows; i++) {
+                matrix[i][0] = 0;
             }
         }
 
@@ -36,6 +52,48 @@ class Matrix {
         }
     }
 }
+
+// Time Complexity = O(2nm)
+// Space Complexity = O(1)
+
+// Better
+
+// class Matrix {
+// public static void main(String[] args) {
+// // int matrix[][] = { { 1, 1, 1 }, { 1, 0, 1 }, { 1, 1, 1 } };
+// int matrix[][] = { { 0, 1 } };
+
+// int rows = matrix.length;
+// int cols = matrix[0].length;
+
+// boolean[] zeroRows = new boolean[rows];
+// boolean[] zeroCols = new boolean[cols];
+
+// for (int i = 0; i < rows; i++) {
+// for (int j = 0; j < cols; j++) {
+// if (matrix[i][j] == 0) {
+// zeroRows[i] = true;
+// zeroCols[j] = true;
+// }
+// }
+// }
+
+// for (int i = 0; i < rows; i++) {
+// for (int j = 0; j < cols; j++) {
+// if (zeroRows[i] || zeroCols[j]) {
+// matrix[i][j] = 0;
+// }
+// }
+// }
+
+// for (int i = 0; i < rows; i++) {
+// for (int j = 0; j < cols; j++) {
+// System.out.print(matrix[i][j] + " ");
+// }
+// System.out.println();
+// }
+// }
+// }
 
 // Time Complexity = O(2nm)
 // Space Complexity = O(n) + O(m)
