668d16a513206c8f50effd0a186117e9db19e5cd
SarthakRathi <sarthakrathi89@gmail.com>
2025-06-22 18:44:21 +0530

    optimal majority element

--- Array/Majority Element 2.java
+++ Array/Majority Element 2.java
@@ -1,26 +1,51 @@
-// Better
+// Optimal
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 class Majority {
     public static void main(String[] args) {
-        int[] nums = { 1 };
+        int[] nums = { 1, 1, 1, 2, 2, 3, 3, 3 };
 
-        Map<Integer, Integer> hash = new HashMap<>();
         List<Integer> answer = new ArrayList<>();
+        int count1 = 0;
+        int count2 = 0;
+        int element1 = Integer.MIN_VALUE;
+        int element2 = Integer.MIN_VALUE;
 
         for (int i = 0; i < nums.length; i++) {
-            if (hash.containsKey(nums[i])) {
-                hash.put(nums[i], hash.get(nums[i]) + 1);
+            if (count1 == 0 && element2 != nums[i]) {
+                count1 = 1;
+                element1 = nums[i];
+            } else if (count2 == 0 && element1 != nums[i]) {
+                count2 = 1;
+                element2 = nums[i];
+            } else if (nums[i] == element1) {
+                count1++;
+            } else if (nums[i] == element2) {
+                count2++;
             } else {
-                hash.put(nums[i], 1);
+                count1--;
+                count2--;
             }
-            if (hash.get(nums[i]) > nums.length / 3 && !answer.contains(nums[i])) {
-                answer.add(nums[i]);
+        }
+
+        count1 = 0;
+        count2 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == element1) {
+                count1++;
             }
+            if (nums[i] == element2) {
+                count2++;
+            }
+        }
+        int min = nums.length / 3;
+        if (count1 > min) {
+            answer.add(element1);
+        }
+        if (count2 > min) {
+            answer.add(element2);
         }
 
         for (Integer integer : answer) {
@@ -28,6 +53,40 @@ class Majority {
         }
     }
 }
+
+// Time Complexity = O(n)
+// Space Complexity = O(1)
+
+// Better
+
+// import java.util.ArrayList;
+// import java.util.HashMap;
+// import java.util.List;
+// import java.util.Map;
+
+// class Majority {
+// public static void main(String[] args) {
+// int[] nums = { 1 };
+
+// Map<Integer, Integer> hash = new HashMap<>();
+// List<Integer> answer = new ArrayList<>();
+
+// for (int i = 0; i < nums.length; i++) {
+// if (hash.containsKey(nums[i])) {
+// hash.put(nums[i], hash.get(nums[i]) + 1);
+// } else {
+// hash.put(nums[i], 1);
+// }
+// if (hash.get(nums[i]) > nums.length / 3 && !answer.contains(nums[i])) {
+// answer.add(nums[i]);
+// }
+// }
+
+// for (Integer integer : answer) {
+// System.out.println(integer);
+// }
+// }
+// }
 
 // Time Complexity = O(n)
 // Space Complexity = O(n)
