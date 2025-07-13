6e29553d97d1fd1168e29ebbbb2743def64141e1
SarthakRathi <sarthakrathi89@gmail.com>
2025-07-13 17:44:49 +0530

    new

--- Linked List/Reverse Linked List.java
+++ Linked List/Reverse Linked List.java
@@ -37,3 +37,20 @@ class ListNode {
 // Space Complexity = O(1)
 
 // Recursive Solution
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseList(head.next);
+
+        ListNode front = head.next;
+        front.next = head;
+        head.next = null;
+
+        return newHead;
+    }
+}
+
+// Time Complexity = O(n)
+// Space Complexity = O(1)
