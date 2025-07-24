c610ed950125cd2deb4fb0840b00fbf72fa36fc6
SarthakRathi <sarthakrathi89@gmail.com>
2025-07-24 18:53:22 +0530

    new

--- Bit Manipulation/Subsets.java
+++ Bit Manipulation/Subsets.java
@@ -1,21 +1,21 @@
 import java.util.ArrayList;
 import java.util.List;
 
-class Solutions {
+class Solution {
     public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        backtrack(result, new ArrayList<>(), nums, 0);
-        return result;
-    }
+        List<List<Integer>> ans = new ArrayList<>();
+        int subsets = 1 << nums.length;
 
-    private void backtrack(List<List<Integer>> result, List<Integer> current, int[] nums, int index) {
-        result.add(new ArrayList<>(current));
-
-        for (int i = index; i < nums.length; i++) {
-            current.add(nums[i]);
-            backtrack(result, current, nums, i + 1);
-            current.remove(current.size() - 1);
+        for (int i = 0; i < subsets; i++) {
+            List<Integer> temp = new ArrayList<>();
+            for (int j = 0; j < nums.length; j++) {
+                if ((i & (1 << j)) != 0)
+                    temp.add(nums[j]);
+            }
+            ans.add(temp);
         }
+
+        return ans;
     }
 }
 
