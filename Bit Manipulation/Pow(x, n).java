d3cce0a53c87bbdbb0cfa59dab58b9d0e021c0ca
SarthakRathi <sarthakrathi89@gmail.com>
2025-07-26 21:09:29 +0530

    new

---
+++ Bit Manipulation/Pow(x, n).java
@@ -0,0 +1,28 @@
+class Solution {
+    public static void main(String[] args) {
+        System.out.println(myPow(2.0, 10));
+    }
+
+    public static double myPow(double x, int n) {
+
+        long bin = n;
+        if (n < 0) {
+            x = 1 / x;
+            bin = -bin;
+        }
+
+        double ans = 1;
+
+        while (bin > 0) {
+            if (bin % 2 == 1)
+                ans *= x;
+            x *= x;
+            bin /= 2;
+        }
+
+        return ans;
+    }
+}
+
+// Time Complexity = O(log2 n)
+// Space Complexity = O(1)
