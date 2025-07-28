5417f030bcb6d4040bdb40855541a9d4a352f854
SarthakRathi <sarthakrathi89@gmail.com>
2025-07-28 18:56:47 +0530

    new

--- Stack and Queue/Implement Stack using Queues.java
+++ Stack and Queue/Implement Stack using Queues.java
@@ -29,11 +29,5 @@ class MyStack {
     }
 }
 
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack obj = new MyStack();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.top();
- * boolean param_4 = obj.empty();
- */
+// Time Complexity = O(n)
+// Time Complexity = O(n)
