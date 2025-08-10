aee58c753529de4cf50d4334013aa4c43ea53b39
SarthakRathi <sarthakrathi89@gmail.com>
2025-08-10 20:18:27 +0530

    new

---
+++ Sliding Window and Two Pointer/Max Consecutive Ones III.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int l = 0;
+        int r;
+
+        for (r = 0; r < nums.length; r++) {
+            if (nums[r] == 0) {
+                k--;
+            }
+            if (k < 0) {
+                if (nums[l] == 0) {
+                    k++;
+                }
+                l++;
+            }
+        }
+        return r - l;
+    }
+}
+
+// Time Complexity = O(n)
+// Space Complexity = O(1)
