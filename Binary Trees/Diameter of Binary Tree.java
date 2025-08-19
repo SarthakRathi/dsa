946975707730286831be9b5f4458001c3dad0ac7
SarthakRathi <sarthakrathi89@gmail.com>
2025-08-19 18:34:53 +0530

    new

---
+++ Binary Trees/Diameter of Binary Tree.java
@@ -0,0 +1,42 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    int max = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        maxDepth(root);
+        return max;
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+
+        int l = maxDepth(root.left);
+        int r = maxDepth(root.right);
+
+        max = Math.max(max, l + r);
+
+        return 1 + Math.max(l, r);
+    }
+}
+
+// Time Complexity = O(n)
+// Space Complexity = O(n)
