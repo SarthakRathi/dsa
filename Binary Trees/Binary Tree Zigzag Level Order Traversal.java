cea0b7bc1ffce0b5d7a76584c8ef3a68bddd590f
SarthakRathi <sarthakrathi89@gmail.com>
2025-08-19 21:51:25 +0530

    new

---
+++ Binary Trees/Binary Tree Zigzag Level Order Traversal.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean leftToRight = true;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            LinkedList<Integer> level = new LinkedList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode currentNode = queue.poll();
+
+                if (leftToRight) {
+                    level.addLast(currentNode.val);
+                } else {
+                    level.addFirst(currentNode.val);
+                }
+
+                if (currentNode.left != null) {
+                    queue.offer(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    queue.offer(currentNode.right);
+                }
+            }
+
+            ans.add(level);
+            leftToRight = !leftToRight;
+        }
+        return ans;
+    }
+}
+
+// Time Complexity = O(n)
+// Space Complexity = O(n)
