554ddab4146b38ba3acd0c8f93ada00798238b2d
SarthakRathi <sarthakrathi89@gmail.com>
2025-08-20 18:53:49 +0530

    new

---
+++ Binary Trees/Symmetric Tree.java
@@ -0,0 +1,36 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return isMirror(root.left, root.right);
+    }
+
+    public boolean isMirror(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+
+        if (p == null || q == null || p.val != q.val) {
+            return false;
+        }
+
+        return isMirror(p.left, q.right) && isMirror(p.right, q.left);
+    }
+}
