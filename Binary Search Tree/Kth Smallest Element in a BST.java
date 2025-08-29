03e8c5b40dcdb0cd500f431de48a43fa11bd11d7
SarthakRathi <sarthakrathi89@gmail.com>
2025-08-29 18:28:52 +0530

    new

---
+++ Binary Search Tree/Kth Smallest Element in a BST.java
@@ -0,0 +1,38 @@
+import java.util.Stack;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        Stack<TreeNode> stack = new Stack<>();
+        while (true) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            k--;
+            if (k == 0) {
+                return root.val;
+            }
+            root = root.right;
+        }
+    }
+}
