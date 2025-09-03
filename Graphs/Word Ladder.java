d380af012730cbdf24458c518a8e8e657be08f54
SarthakRathi <sarthakrathi89@gmail.com>
2025-09-03 21:13:37 +0530

    new

---
+++ Graphs/Word Ladder.java
@@ -0,0 +1,51 @@
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
+class Pair {
+    String first;
+    int second;
+
+    Pair(String first, int second) {
+        this.first = first;
+        this.second = second;
+    }
+}
+
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(beginWord, 1));
+
+        Set<String> st = new HashSet<>();
+        int len = wordList.size();
+        for (int i = 0; i < len; i++) {
+            st.add(wordList.get(i));
+        }
+        st.remove(beginWord);
+
+        while (!q.isEmpty()) {
+            String word = q.peek().first;
+            int steps = q.peek().second;
+            q.poll();
+
+            if (word.equals(endWord))
+                return steps;
+
+            for (int i = 0; i < word.length(); i++) {
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    char[] replacedCharArray = word.toCharArray();
+                    replacedCharArray[i] = ch;
+                    String replacedWord = new String(replacedCharArray);
+                    if (st.contains(replacedWord)) {
+                        st.remove(replacedWord);
+                        q.add(new Pair(replacedWord, steps + 1));
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+}
